65c4ee812e442f24f6b96a5147d811d900f20cea
robin <robenkaraa29@gmail.com>
2020-06-27 22:02:24 +0300

    fixed cpu interface and started with the debug interface and ppu

---
+++ interface.h
@@ -0,0 +1,11 @@
+#ifndef NESX_INTERFACE_H
+#define NESX_INTERFACE_H
+
+#define W 128
+#define H 128
+
+int init_interface() ;
+void update_interface () ;
+void copy_pattern_table() ;
+
+#endif //NESX_INTERFACE_H
